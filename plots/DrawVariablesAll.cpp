244eb10d4c4a23102d7ad9e4b81b53c9c5663f96
Fabio Iemmi <fabio.iemmi@cern.ch>
2025-07-31 18:03:21 +0200

    new DrawVariablesAll.cpp, just for reference

--- plots/DrawVariablesAll.cpp
+++ plots/DrawVariablesAll.cpp
@@ -1,83 +1,50 @@
+// Auto-generated DrawVariable calls
 #include "DrawVariable.cpp"
-void DrawVariablesAll(bool PRINT)
-{
-  //inclusive bins
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","#gamma #gamma preselection","","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg25",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 25 GeV","","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg25_etal1p5",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 25 GeV;","|#eta_{#gamma}| < 1.5","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg25_etag1p5",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 25 GeV;","|#eta_{#gamma}| > 1.5","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg35",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 35 GeV","","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg35_etal1p5",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 35 GeV;","|#eta_{#gamma}| < 1.5","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg35_etag1p5",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 35 GeV;","|#eta_{#gamma}| > 1.5","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg50",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 50 GeV","","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg50_etal1p5",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 50 GeV;","|#eta_{#gamma}| < 1.5","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass","Run2","Zmumug_ptg50_etag1p5",false,1,5,80,100,0.4,1.3,"m_{#mu#mu#gamma} [GeV]","p_{T.#gamma} > 50 GeV;","|#eta_{#gamma}| > 1.5","","#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
 
-  //exclusive bins (thanks ChatGPT)
-  // pt in [20, 35)
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "|#eta_{#gamma}| < 1.5", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "|#eta_{#gamma}| > 1.5", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "|#eta_{#gamma}| > 1.5", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [20, 35) GeV", "|#eta_{#gamma}| > 1.5", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-
-  // pt in [35, 50)
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "|#eta_{#gamma}| < 1.5", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "|#eta_{#gamma}| > 1.5", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "|#eta_{#gamma}| > 1.5", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #in [35, 50) GeV", "|#eta_{#gamma}| > 1.5", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-
-  // pt ≥ 50
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "|#eta_{#gamma}| < 1.5", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "|#eta_{#gamma}| > 1.5", "", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "|#eta_{#gamma}| > 1.5", "R9 < 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-  DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3,
-             "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} > 50 GeV", "|#eta_{#gamma}| > 1.5", "R9 #geq 0.96", "#chi^{2}/N_{d.o.f.} = ", false, 505, PRINT);
-
+void DrawVariablesAll(bool PRINT) {
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "|#eta_{#gamma}| #geq 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "|#eta_{#gamma}| < 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt20to35_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [20, 35) GeV;", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "|#eta_{#gamma}| #geq 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "|#eta_{#gamma}| < 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_pt35to50_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T,#gamma} #in [35, 50) GeV;", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "|#eta_{#gamma}| #geq 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "|#eta_{#gamma}| < 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg20_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 20 GeV;", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "|#eta_{#gamma}| #geq 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "|#eta_{#gamma}| < 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg25_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 25 GeV;", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "|#eta_{#gamma}| #geq 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "|#eta_{#gamma}| < 1.5", "", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9g0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9g0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9g0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "|#eta_{#gamma}| < 1.5", "R9 #geq 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9l0p96", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9l0p96_etag1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "|#eta_{#gamma}| #geq 1.5", "R9 < 0.96", "", false, 505, PRINT);
+    DrawVariable("CMS_mumug_mass", "Run2", "Zmumug_ptg50_r9l0p96_etal1p5", false, 1, 5, 80, 100, 0.4, 1.3, "m_{#mu#mu#gamma} [GeV]", "p_{T.#gamma} #geq 50 GeV;", "|#eta_{#gamma}| < 1.5", "R9 < 0.96", "", false, 505, PRINT);
 }
-
-
-
-
-
-
